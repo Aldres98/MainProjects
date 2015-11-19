c49c37b921de5f184db723621e5a9e083900cc88
Aldres98 <aldres98@gmail.com>
2015-11-19 16:13:30 +0400

    Reading from DB

--- db.rb
+++ db.rb
@@ -2,7 +2,10 @@ require 'sqlite3'
 
 db = SQLite3::Database.new 'test.sqlite'
 
-db.execute "Insert into Cars (Name, Price) VALUES ('Jaguar', 77777)"
+db.execute "SELECT * FROM Cars" do |car|
+	puts car
+	puts "===="
+end
 
 
 
